2506c3d09d38af4182f0c6ef7103482d0c81fc0a
Patrick Kim <patrick.yrkim@gmail.com>
2023-05-02 23:59:30 -0700

    Add login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import './App.css'
+import { LoginPage } from './Routes.js'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='login' element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/Routes.js
@@ -0,0 +1,5 @@
+import LoginPage from '../src/pages/LoginPage.jsx'
+
+export {
+    LoginPage,
+}
